1068358a2956f5f8eb5eea3ca8ab8029b3e1b6b4
ahmadjoba96 <ahmad.joba.cs@gmail.com>
2026-08-22 13:03:12 +0300

    fix: remove duplicate migration creating same index twice

--- migrations/1787340796046_add-service-timestamp-index.js
+++
@@ -1,9 +0,0 @@
-export const shorthands = undefined;
-
-export const up = (pgm) => {
-  pgm.createIndex('logs', ['service', 'timestamp']);
-};
-
-export const down = (pgm) => {
-  pgm.dropIndex('logs', ['service', 'timestamp']);
-};
